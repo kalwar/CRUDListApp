897582c9a07147c02600fb67d37ac5664175ab25
Administrator <sk@unelma.ee>
2022-05-03 18:55:22 +0300

    Initial CRUDListApp in Symfony

---
+++ README.md
@@ -0,0 +1,24 @@
+# CrudListApp
+
+CrudListApp is a very simple app for CRUD operations in Symfony 6.
+
+---
+
+# Steps to follow
+
+1. Simply run the app using Symfony command
+2. First do, `composer install` and `npm install` to install dependencies
+3. Symfony command: `symfony server:start -d` to start in daemon mode
+4. Visit URL: https://127.0.0.1:8000/ to run the app
+5. Note: You will need to have local database setup to run this app
+
+# Tech stack
+
+1.  [Symfony](https://symfony.com/)
+2.  [PHP](https://www.php.net/)
+3.  [MySQL](https://mysql.com)
+4.  [phpMyAdmin](https://www.phpmyadmin.net/)
+
+# Use for reference
+
+Use solely for reference material only
---
+++ src/Controller/CrudListController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CrudListController extends AbstractController
+{
+    #[Route('/crud/list', name: 'app_crud_list')]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $tasks = $em->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+        return $this->render('crud_list/index.html.twig', ['tasks' => $tasks]);
+    }
+
+    #[Route('/create', name: 'create_task', methods: ['POST'])]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $title = trim($request->get("title"));
+        if (empty($title))
+            return $this->redirectToRoute('app_crud_list');
+
+        $entityManager = $doctrine->getManager();
+        $task = new Task;
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_crud_list');
+    }
+
+    #[Route('/update/{id}', name: 'update_task')]
+    public function update($id, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+        $entityManager->flush();
+        return $this->redirectToRoute('app_crud_list');
+    }
+
+    #[Route('/delete/{id}', name: 'delete_task')]
+    public function delete($id, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $id = $entityManager->getRepository(Task::class)->find($id);
+        $entityManager->remove($id);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_crud_list');
+    }
+}
